0d67ac5a68f0e96666692274751ef6fead904af2
rogerbaracy <rogerbaracy.s@gmail.com>
2026-04-10 02:02:15 -0300

    feat: ajustado para atualizar o github page quando a branch master sofrer alterações

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/mimojs/",
+
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: "src/main.ts",
+      name: "mimojs",
+      fileName: "mimojs",
+      formats: ["es", "umd"],
+    },
+  },
+
   server: {
     host: true,
-    port: 5173
+    port: 5173,
   },
-  build: {
-    lib: {
-      entry: 'src/main.ts',
-      name: 'mimojs',
-      fileName: 'mimojs',
-      formats: ['es', 'umd']
-    }
-  }
-})
+});
